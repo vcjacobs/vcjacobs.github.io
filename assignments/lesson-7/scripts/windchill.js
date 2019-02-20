0e4e1a957dd249fcff01ced9a1ca083727f1c52e
vcjacobs <vcjacobs97@gmail.com>
2019-02-20 12:50:23 -0700

    created lesson 7

---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,6 @@
+var temp = 50;
+var windSpeed = 10;
+
+var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed,0.16) + 0.4275 * temp * Math.pow(windSpeed,0.16);
+
+document.getElementById("id-from-html").innerHTML = temp + "&deg;"
