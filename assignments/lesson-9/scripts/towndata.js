896a8c702e91852a719fff89264cd975178ff1d5
vcjacobs <vcjacobs97@gmail.com>
2019-03-23 18:58:46 -0600

    finished home page

--- assignments/lesson-9/scripts/towndata.js
+++ assignments/lesson-9/scripts/towndata.js
@@ -1,54 +1,41 @@
-var header = document.querySelector('header');
+
 var section = document.querySelector('section');
 
-var requestURl = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+var requestURl = 'https://byui-cit230.github.io/weather/data/towndata.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURl);
 request.responseType = 'json';
 request.send();
+
 request.onload = function () {
-    var superHeroes = request.response;
-    populateHeader(superHeroes);
-    showHeroes(superHeroes);
+    var townData = request.response;
+    showTowns(townData);
 }
-function populateHeader(jsonObj) {
-    var myH1 = document.createElement('h1');
-    myH1.textContent = jsonObj['squadName'];
-    header.appendChild(myH1);
 
-    var myPara = document.createElement('p');
-    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-    header.appendChild(myPara);
-}
-function showHeroes(jsonObj) {
-    var heroes = jsonObj['members'];
+function showTowns(jsonObj) {
+    var towns = jsonObj['towns'];
 
-    for (var i = 0; i < heroes.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myList = document.createElement('ul');
 
-        myH2.textContent = heroes[i].name;
-        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-        myPara2.textContent = 'Age: ' + heroes[i].age;
-        myPara3.textContent = 'Superpowers:';
+    for (var i = 0; i < towns.length; i++) {
+        if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven") {
+            var myArticle = document.createElement('article');
+            var myPara1 = document.createElement('p');
+            var myPara2 = document.createElement('p');
+            var myPara3 = document.createElement('p');
+            var myPara4 = document.createElement('p');
 
-        var superPowers = heroes[i].powers;
-        for (var j = 0; j < superPowers.length; j++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = superPowers[j];
-            myList.appendChild(listItem);
+            myPara1.textContent = 'Motto: ' + towns[i].motto;
+            myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            myPara3.textContent = 'Current Population: ' + towns[i].currentPopulation;
+            myPara4.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+
+
+            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myPara2);
+            myArticle.appendChild(myPara3);
+            myArticle.appendChild(myPara4);
+
+            section.appendChild(myArticle);
         }
-
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-        myArticle.appendChild(myList);
-
-        section.appendChild(myArticle);
     }
 }
